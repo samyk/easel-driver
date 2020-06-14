abd7d72c8bfb8c98df598e64242c816a7fbe7df3
Per Ivar Nerseth <perivar@nerseth.com>
2020-06-14 02:28:12 +0200

    added back the needed exports

--- easel-driver.sh
+++ easel-driver.sh
@@ -49,7 +49,10 @@ EOF
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash - &&
 
 # Rerun Profile script to start NVM
-. ~/.bashrc && # Rerun profile after installing nvm
+export NVM_DIR="$HOME/.nvm" &&
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" && # This loads nvm bash_completion
+. ~/.bashrc &&
 
 # Install nodejs lts
 nvm install --lts &&
